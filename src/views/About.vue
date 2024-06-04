46518bec7e20de19dbedb15c1962ce288be3f838
Niko <y111626395@outlook.com>
2024-06-04 22:01:42 +0800

    添加

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,31 @@
 
   <div>
     <h1>
-      <button @click="bilibili">这是关于界面</button></h1>
+      <button @click="submit()">这是关于界面</button></h1>
+      <p1>用户名：{{ username }}</p1><br>
+      <p1>用户昵称：{{ nickname }}</p1><br>
   </div>
 </template>
 
 <script setup lang="ts">
-const bilibili = () =>{
-  window.location.replace("https://www.bilibili.com")
+import { getUserData } from '../api/user'
+import { ref } from 'vue'
+
+
+const username = ref('');
+const nickname = ref('');
+
+const submit = async () =>{
+  
+  console.log('token',localStorage.getItem('token'))
+  const response = await getUserData();
+
+  if(response.code == 200){
+    username.value = response.data.username
+    nickname.value = response.data.nick
+  }else{
+    console.log('fail')
+  }
 }
 </script>
 
